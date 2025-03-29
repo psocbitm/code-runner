63fea41ce67e4fc601142f9bb9e1e1b3092a5197
sand-storm-17 <sandeeepkumaar.0034@gmail.com>
2025-03-29 17:07:15 +0530

    added post api to submit code and send code to redis

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
 import { startServer } from "./src/server.js"
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', function() {
+    console.log('Connected!');
+});
 
 startServer()
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,33 @@
-import express from "express"
+import express from "express";
+
+import { v4 as uuidv4 } from "uuid";
+import client from "./config/redis.js";
 
 export async function startServer() {
-  const app = express()
-  const port = process.env.PORT || 3000
+  const app = express();
+  app.use(express.json());
+
+  const redisClient = client;
+
+  const port = process.env.PORT || 3000;
 
   app.get("/", (req, res) => {
-    res.send("Hello World!")
-  })
+    res.send("Hello World!");
+  });
+
+  app.post("/submit-code", async (req, res) => {
+    const { language, code } = req.body;
+    const job = { language, code: Buffer.from(code, "utf-8"), id: uuidv4() };
+    try {
+      await redisClient.lPush("codeQueue", JSON.stringify(job));
+      res.json({ message: "Code submitted successfully", job });
+    } catch (err) {
+      console.error("Redis Error:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+    console.log(`Server is running on port ${port}`);
+  });
 }
